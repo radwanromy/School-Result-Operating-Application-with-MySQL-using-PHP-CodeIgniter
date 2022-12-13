afcf5ee4dacc47e8ce8f20eee38a2215b30090ac
A . S . M . RADWAN <50805604+radwanromy@users.noreply.github.com>
2022-12-13 10:28:13 +0600

    add 01

---
+++ application/views/add.php
@@ -0,0 +1,52 @@
+<html>
+
+<head>
+    <title>codeigniter Tutorial</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+</head>
+
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-3"></div>
+            <div class="col-md-6">
+                <form method="post" name="frmAdd" action="">
+                    <h3>Add User</h3>
+
+                    <div class="form-group">
+                        <label for="">First Name</label>
+                        <input type="text" class="form-control" name="txtFname">
+                    </div>
+
+                    <div class="form-group">
+                        <label for="">Last Name</label>
+                        <input type="text" class="form-control" name="txtLname">
+                    </div>
+
+                    <div class="form-group">
+                        <label for="">Address</label>
+                        <input type="text" class="form-control" name="txtAddress">
+                    </div>
+
+                    <div class="form-group">
+                        <label for="">Email</label>
+                        <input type="text" class="form-control" name="txtEmail">
+                    </div>
+
+                    <div class="form-group">
+                        <label for="">Mobile</label>
+                        <input type="text" class="form-control" name="txtMobile">
+                    </div>
+
+                    <div class="form-group">
+                        <input type="submit" value="Add user now" name="btnadd" class="btn btn-primary btn-lg">
+                    </div>
+
+                </form>
+                <div class="col-md-3"></div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
