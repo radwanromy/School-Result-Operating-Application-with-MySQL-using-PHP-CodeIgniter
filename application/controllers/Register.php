e98e7de1b939fd4fa5aa90cc6f5e5f5ac0444ddd
A . S . M . RADWAN <50805604+radwanromy@users.noreply.github.com>
2022-12-13 10:21:47 +0600

    regi 01

---
+++ application/controllers/Register.php
@@ -0,0 +1,64 @@
+<?php
+
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Register extends CI_Controller
+{
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->helper('url');
+        $this->load->model('register_model');
+    }
+
+    public function index()
+    {
+        $arrData['register_detail'] = $this->register_model->get_all_register_detail();
+        $this->load->view('list', $arrData);
+    }
+
+    public function add()
+    {
+        if ($this->input->post('btnadd')) {
+            $arrData['first_name'] = $this->input->post('txtFname');
+            $arrData['last_name'] = $this->input->post('txtLname');
+            $arrData['address'] = $this->input->post('txtAddress');
+            $arrData['email'] = $this->input->post('txtEmail');
+            $arrData['mobile'] = $this->input->post('txtMobile');
+
+            $insert = $this->register_model->insert($arrData);
+            if ($insert) {
+                redirect('register');
+            }
+        }
+        $this->load->view('add');
+    }
+
+    public function edit($id)
+    {
+        $arrData['register_detail'] = $this->register_model->get_id_wise_register_detail($id);
+
+        if ($this->input->post('btnEdit')) {
+            $editData['first_name'] = $this->input->post('txtFname');
+            $editData['last_name'] = $this->input->post('txtLname');
+            $editData['address'] = $this->input->post('txtAddress');
+            $editData['email'] = $this->input->post('txtEmail');
+            $editData['mobile'] = $this->input->post('txtMobile');
+
+            $update = $this->register_model->update($editData, $id);
+            if ($update) {
+                redirect('register');
+            }
+        }
+        $this->load->view('edit', $arrData);
+    }
+
+    public function delete($id)
+    {
+        $delete = $this->register_model->delete($id);
+        if ($delete) {
+            redirect('register');
+        }
+    }
+}
